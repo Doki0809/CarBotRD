3e5589fc867f56cfd20f34d8c03fd8bf040f2af5
Jean 🙃 <jean@MacBook-Air-de-Jean.local>
2025-12-27 20:55:21 -0400

    Version final ahora si

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCAkn8JxrTEyJ_fgA8VVbbheTMPm4lCjH4",
   authDomain: "carbot-5d709.firebaseapp.com",
@@ -16,6 +13,12 @@ const firebaseConfig = {
   measurementId: "G-WPR5MC5J0L"
 };
 
-// Initialize Firebase
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Exportar herramientas para usar en la App
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export default app;
